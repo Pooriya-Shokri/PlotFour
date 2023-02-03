3e33f574e9fe54d5df45dc6e45554bfe69f76c63
pooriya <pooriya.shokri@gmail.com>
2023-02-03 11:35:01 +0330

    Implemented SimpleBoardCreator

--- src/main/java/me/pooriya/plotfour/board/SimpleBoardCreator.java
+++ src/main/java/me/pooriya/plotfour/board/SimpleBoardCreator.java
@@ -4,6 +4,8 @@ public class SimpleBoardCreator implements BoardCreator {
 
 	@Override
 	public int[][] createBoard(int rows, int columns) {
+		if (rows * columns != 0)
+			return new int[rows][columns];
 		return new int[0][];
 	}
 
--- src/test/java/me/pooriya/plotfour/board/SimpleBoardCreatorTest.java
+++ src/test/java/me/pooriya/plotfour/board/SimpleBoardCreatorTest.java
@@ -7,10 +7,25 @@ import static org.junit.Assert.assertEquals;
 public class SimpleBoardCreatorTest {
 
 	@Test
-	public void shouldReturnEmptyBoard() {
+	public void shouldReturnEmptyBoard_whenColOrRowAreZero() {
 		SimpleBoardCreator creator = new SimpleBoardCreator();
-		int[][] result = creator.createBoard(0, 0);
-		assertEquals(0, result.length);
+		int[][] result1 = creator.createBoard(0, 0);
+		assertEquals(0, result1.length);
+
+		int[][] result2 = creator.createBoard(10, 0);
+		assertEquals(0, result2.length);
+
+		int[][] result3 = creator.createBoard(0, 10);
+		assertEquals(0, result3.length);
+	}
+
+	@Test
+	public void shouldReturnNormalBoard() {
+		SimpleBoardCreator creator = new SimpleBoardCreator();
+		int[][] result = creator.createBoard(2, 3);
+		assertEquals(2, result.length);
+		assertEquals(3, result[0].length);
+		assertEquals(3, result[1].length);
 	}
 
 }
