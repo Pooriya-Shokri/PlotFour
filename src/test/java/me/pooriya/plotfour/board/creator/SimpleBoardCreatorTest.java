9df2f700a92f3c0271810d0b126a6cb4c4ed336d
pooriya <pooriya.shokri@gmail.com>
2023-02-03 11:56:18 +0330

    Added BoardSpecification to SimpleBoardCreatorTest

--- src/test/java/me/pooriya/plotfour/board/creator/SimpleBoardCreatorTest.java
+++ src/test/java/me/pooriya/plotfour/board/creator/SimpleBoardCreatorTest.java
@@ -1,5 +1,6 @@
 package me.pooriya.plotfour.board.creator;
 
+import me.pooriya.plotfour.board.BoardSpecification;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,20 +10,20 @@ public class SimpleBoardCreatorTest {
 	@Test
 	public void shouldReturnEmptyBoard_whenColOrRowAreZero() {
 		SimpleBoardCreator creator = new SimpleBoardCreator();
-		int[][] result1 = creator.createBoard(0, 0);
+		int[][] result1 = creator.createBoard(BoardSpecification.of(0, 0));
 		assertEquals(0, result1.length);
 
-		int[][] result2 = creator.createBoard(10, 0);
+		int[][] result2 = creator.createBoard(BoardSpecification.of(10, 0));
 		assertEquals(0, result2.length);
 
-		int[][] result3 = creator.createBoard(0, 10);
+		int[][] result3 = creator.createBoard(BoardSpecification.of(0, 10));
 		assertEquals(0, result3.length);
 	}
 
 	@Test
 	public void shouldReturnNormalBoard() {
 		SimpleBoardCreator creator = new SimpleBoardCreator();
-		int[][] result = creator.createBoard(2, 3);
+		int[][] result = creator.createBoard(BoardSpecification.of(2, 3));
 		assertEquals(2, result.length);
 		assertEquals(3, result[0].length);
 		assertEquals(3, result[1].length);
