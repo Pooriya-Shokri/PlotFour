ecdc88e5105c5cffe625360e193e1bb2c93918c2
pooriya <pooriya.shokri@gmail.com>
2023-02-04 14:42:44 +0330

    added turn handling to Board

--- src/main/java/me/pooriya/plotfour/board/Board.java
+++ src/main/java/me/pooriya/plotfour/board/Board.java
@@ -4,14 +4,33 @@ import lombok.NonNull;
 import lombok.Value;
 import me.pooriya.plotfour.player.Player;
 
+import static me.pooriya.plotfour.board.Board.TurnResult.*;
+
 @Value(staticConstructor = "of")
 public class Board {
 
+	@NonNull
 	Player[][] state;
 
 	@NonNull
 	BoardSpecification spec;
 
+	public TurnResult turn(Player player, int col) {
+		if (col > spec.getColumns() || col < 1)
+			return INVALID_COLUMN;
+		int  i = 0;
+		while ( i < spec.getRows() && state[i][col-1] != null)
+			i++;
+		if (i >= spec.getRows())
+			return FULL_COLUMN;
+		state[i][col -1] = player;
+		return SUCCESS;
+	}
 
+	public enum TurnResult {
+		SUCCESS,
+		FULL_COLUMN,
+		INVALID_COLUMN
+	}
 
 }
